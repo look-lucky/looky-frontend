e6aa965021c685ea412685aa46642c588f3f4971
dlcodudco <dlcodudco@jbnu.ac.kr>
2026-03-03 17:50:14 +0900

    fix: 점주 가게 관리 페이지 오류 수정

--- app.config.ts
+++ app.config.ts
@@ -24,84 +24,85 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: bundleId,
+      usesAppleSignIn: true,
     },
     android: {
       package: bundleId,
-    adaptiveIcon: {
-      backgroundColor: "#FEF5E5",
-      foregroundImage: "./assets/images/logo/ios-looky.png",
+      adaptiveIcon: {
+        backgroundColor: "#FEF5E5",
+        foregroundImage: "./assets/images/logo/ios-looky.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
     },
-    edgeToEdgeEnabled: true,
-    predictiveBackGestureEnabled: false,
-  },
-  web: {
-    output: "static",
-    favicon: "./assets/images/favicon.png",
-  },
-  plugins: [
-    "expo-router",
-    [
-      "expo-splash-screen",
-      {
-        image: "./assets/images/splash-icon.png",
-        imageWidth: 200,
-        resizeMode: "contain",
-        backgroundColor: "#ffffff",
-        dark: {
-          backgroundColor: "#000000",
-        },
-      },
-    ],
-    "expo-secure-store",
-    [
-      "@mj-studio/react-native-naver-map",
-      {
-        client_id: process.env.EXPO_PUBLIC_NAVER_MAP_CLIENT_ID ?? "",
-      },
-    ],
-    [
-      "expo-build-properties",
-      {
-        android: {
-          kotlinVersion: "2.1.20",
-          extraMavenRepos: [
-            "https://repository.map.naver.com/archive/maven",
-            "https://devrepo.kakao.com/nexus/content/groups/public/",
-          ],
-        },
-      },
-    ],
-    "expo-web-browser",
-    "expo-image-picker",
-    "@react-native-community/datetimepicker",
-    [
-      "@react-native-seoul/kakao-login",
-      {
-        kakaoAppKey: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY ?? "",
-      },
-    ],
-    [
-      "@react-native-google-signin/google-signin",
-      {
-        iosUrlScheme: "com.googleusercontent.apps.409232942871-dardm07iqdd0pfmhvjod9gnsets1g520",
-      },
-    ],
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
-  updates: {
-    url: "https://u.expo.dev/554fbeb0-4c38-4f44-86c2-6591b905ee36",
-  },
-  runtimeVersion: {
-    policy: "appVersion",
-  },
-  extra: {
-    router: {},
-    eas: {
-      projectId: "554fbeb0-4c38-4f44-86c2-6591b905ee36",
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
-  },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      "expo-secure-store",
+      [
+        "@mj-studio/react-native-naver-map",
+        {
+          client_id: process.env.EXPO_PUBLIC_NAVER_MAP_CLIENT_ID ?? "",
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          android: {
+            kotlinVersion: "2.1.20",
+            extraMavenRepos: [
+              "https://repository.map.naver.com/archive/maven",
+              "https://devrepo.kakao.com/nexus/content/groups/public/",
+            ],
+          },
+        },
+      ],
+      "expo-web-browser",
+      "expo-image-picker",
+      "@react-native-community/datetimepicker",
+      [
+        "@react-native-seoul/kakao-login",
+        {
+          kakaoAppKey: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY ?? "",
+        },
+      ],
+      [
+        "@react-native-google-signin/google-signin",
+        {
+          iosUrlScheme: "com.googleusercontent.apps.409232942871-dardm07iqdd0pfmhvjod9gnsets1g520",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    updates: {
+      url: "https://u.expo.dev/554fbeb0-4c38-4f44-86c2-6591b905ee36",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "554fbeb0-4c38-4f44-86c2-6591b905ee36",
+      },
+    },
   };
 };
